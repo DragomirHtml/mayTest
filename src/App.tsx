1a61a34c5b13cf190120deb6bcda7f493b9cfe59
DragomirHtml <dragomir13042007@gmail.com>
2026-03-28 16:11:31 +0300

    список дел задание

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,23 @@ import { Log } from './components/Log'
 import { Cylinder } from './components/Cylinder'
 
 import { RandomSum } from './components/RandomSum';
+import { UserProfile } from './components/UserProfile'
 
 import './App.css'
+import { StepCounter } from './components/StepCounter'
+import { Todolist } from './components/Todolist'
 
 function App() {
   return (
     <>
-      <Energy />
-      <Pythagras />
-      <Cylinder />
-      <Quadra />
-      <Quadrasum />
-      <Water /> 
-      <Log />
       <RandomSum />
+      <UserProfile 
+      firstname={'Диана'}
+      lastname={'Хазикова'}
+      birthyear={2006}
+      />
+      <StepCounter />
+      <Todolist/>
     </>
   )
 }
