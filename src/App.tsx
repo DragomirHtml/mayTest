128ae0eac8dfea28028fb8b690a61e8337bf897f
DragomirHtml <dragomir13042007@gmail.com>
2026-04-11 14:48:28 +0300

    Added timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import { Energy } from './components/Energy'
-import { Pythagras } from './components/Pythagpras'
-import { Quadra } from './components/Quadra'
-import { Quadrasum } from './components/Quadrasum'
-import { Water } from './components/Water'
-import { Log } from './components/Log'
-import { Cylinder } from './components/Cylinder'
-
-import { RandomSum } from './components/RandomSum';
-import { UserProfile } from './components/UserProfile'
-
-import './App.css'
-import { StepCounter } from './components/StepCounter'
-import { Todolist } from './components/Todolist'
-
+import { Timer } from "./components/Timer"
 function App() {
   return (
     <>
-      <RandomSum />
-      <UserProfile 
-      firstname={'Диана'}
-      lastname={'Хазикова'}
-      birthyear={2006}
-      />
-      <StepCounter />
-      <Todolist/>
+  <Timer/>
     </>
   )
 }
